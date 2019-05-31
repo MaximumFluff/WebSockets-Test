be6f09a7fa741fce280af197e3bf8d10580c962d
Alex Jacobs <alexander.jacobs@industry62.fi>
2019-05-31 22:03:58 +0300

    refactored away frontend

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const socket = require('socket.io');
 const app = express();
 const port = process.env.port || 4000;
 
-app.use(express.static('public'));
-
 const server = app.listen(port, () => {
   console.log("Listening on port", port);
 });
